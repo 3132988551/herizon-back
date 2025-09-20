9fad8c1465b2b099a688532c6399ac2338aa064c
zz` <2536026346@qq.com>
2025-09-20 16:20:12 +0800

    初始化后端

---
+++ src/test/java/org/example/herizon/HerizonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.herizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerizonApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
